78294c9650ac9e2f203c0bfa0508da614f2a1dc0
Elias <eliasneto072@gmail.com>
2024-05-13 20:20:42 -0300

    added forms

--- app_cloud/admin.py
+++ app_cloud/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Room, Topic, Message, User
+from .models import Room, Topic, Message
+#from django.contrib.auth.models import User
 
-admin.site.register(User)
+#admin.site.register(User)
 admin.site.register(Room)
 admin.site.register(Topic)
 admin.site.register(Message)
